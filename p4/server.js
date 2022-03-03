6eb43afba3e78c0a150564c6d1240191fe39b3f4
shecoomontano <shecoomontano@gmail.com>
2022-03-03 11:55:44 -0600

    Clase motores de vistas

--- p4/server.js
+++ p4/server.js
@@ -1,19 +1,26 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const { appConfig } = require('./lib/config')
 const bodyParser = require('body-parser')
+const md5 = require('md5')
+const { appConfig } = require('./lib/config')
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.set('view engine', 'pug');
-
 
 app.get('/', function(req, res) {
     res.render('index');
 });
 
+app.get('/practica', function(req, res) {
+    res.render('practica');
+});
+
 app.post('/ingreso', function(req, res) {
-    res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+    //res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+    let pass = md5(req.body.pass)
+    res.render('ingreso', {correo: req.body.email, pass: pass});
 });
 
 app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`)) 
