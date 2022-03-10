97f255332cf68b187284945bfcf611dafce8f47a
shecoomontano <shecoomontano@gmail.com>
2022-03-10 13:46:43 -0600

    clase, registro de usuarios y vista

--- p4/server.js
+++ p4/server.js
@@ -5,6 +5,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const md5 = require('md5')
 const { appConfig } = require('./lib/config')
+const connection = require('./db/mysql.js')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'pug');
@@ -13,14 +14,33 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/practica', function(req, res) {
-    res.render('practica');
+app.get('/registro', function(req, res) {
+    res.render('registro');
 });
 
-app.post('/ingreso', function(req, res) {
-    //res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+app.post('/registroUser', function(req, res) {
+    if(req.body.registro == ""){
+        let pass  = md5(req.body.pass) //Ciframos la contraseña.
+        //Realizamos la query del insert de los datos enviados.
+        connection.query('INSERT INTO users (username, email, pass) VALUES (?, ?, ?)',[req.body.username, req.body.email, pass], function(err, result, fields){
+            
+            if (err) throw err; //Si hay un error, lo muestra
+            res.redirect('/')
+            
+        })
+       }else{
+        res.redirect('/')
+       }
+});
+
+app.post('/auth', function(req, res) {
+
+    if(req.body.sesion == ""){
     let pass = md5(req.body.pass)
-    res.render('ingreso', {correo: req.body.email, pass: pass});
+       res.render('ingreso', {correo: req.body.email, pass: pass});
+       }else{
+           res.redirect('/registro')
+       }
 });
 
 app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`)) 
